0eba3e95035adae57edba16f8e6bea50574ead50
Roky3029 <mikirb30@gmail.com>
2024-01-23 16:07:28 +0100

    v1.0 -> Ready for deployment·

--- server.py
+++ server.py
@@ -14,4 +14,4 @@ for name, blueprint in module.__dict__.items():
         app.register_blueprint(blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
